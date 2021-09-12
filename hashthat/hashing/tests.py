d56d0c3589c7b339871f9395f19e66d13fbeddea
André Lopes <lopes.andre@outlook.com>
2021-09-12 12:43:19 -0300

    First Django test: test homepage

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from selenium import webdriver
 
-# Create your tests here.
+class FunctionalTestCase(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def test_there_is_homepage(self):
+        '''
+        Tests the home page for having the text install as it
+        does in the Django regular install page.
+        '''
+        self.browser.get('http://localhost:8000')
+        self.assertIn('install', self.browser.page_source)
+
+    def tearDown(self):
+        self.browser.quit()
