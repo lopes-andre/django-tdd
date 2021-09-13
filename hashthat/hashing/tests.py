5ae385367ad6ff9df1aac00b33d99c1c53d0d401
André Lopes <lopes.andre@outlook.com>
2021-09-13 19:53:34 -0300

    Add Functional Test: test_bad_data

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -1,39 +1,52 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 from selenium import webdriver
 from .forms import HashForm
 from .models import Hash
 import hashlib
 
-# class FunctionalTestCase(TestCase):
+class FunctionalTestCase(TestCase):
 
-#     def setUp(self):
-#         self.browser = webdriver.Firefox()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-#     def test_there_is_homepage(self):
-#         '''
-#         Tests the home page for having the text an specific
-#         text on the page source.
-#         '''
-#         self.browser.get('http://localhost:8000')
-#         self.assertIn('Enter your text to be encoded to a hash:', self.browser.page_source)
+    def test_there_is_homepage(self):
+        '''
+        Tests the home page for having the text an specific
+        text on the page source.
+        '''
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Enter your text to be encoded to a hash:', self.browser.page_source)
 
-#     def test_hash_of_hello(self):
-#         '''
-#         Tests finding a form text element by its id, filling it
-#         with a 'hello' string and submiting it; then asserts for
-#         its correspondent hash in the home page source.
-#         '''
-#         self.browser.get('http://localhost:8000')
-#         text = self.browser.find_element_by_id('id_text')
-#         text.send_keys('hello')
-#         self.browser.find_element_by_name('submit').click()
-#         self.assertIn(
-#             '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
-#             self.browser.page_source
-#         )
+    def test_hash_of_hello(self):
+        '''
+        Tests finding a form text element by its id, filling it
+        with a 'hello' string and submiting it; then asserts for
+        its correspondent hash in the home page source.
+        '''
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        self.browser.find_element_by_name('submit').click()
+        self.assertIn(
+            '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
+            self.browser.page_source
+        )
 
-#     def tearDown(self):
-#         self.browser.quit()
+    def test_bad_data(self):
+        '''
+        Tries to create a hash with a length greater than the max_chars
+        allowed on the models. Tests for a ValidationError.
+        '''
+        def badHash():
+            hash = Hash()
+            hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824xxx'
+            hash.full_clean()
+
+        self.assertRaises(ValidationError, badHash)
+
+    def tearDown(self):
+        self.browser.quit()
 
 class UnitTestCase(TestCase):
 
